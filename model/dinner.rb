92006f36bc2d3dfcf436273b51aff6608c1ae84a
Bryan Holdt <BHoldt@paraport.com>
2015-07-07 13:45:02 -0700

    Removed broken phone number validation

--- model/dinner.rb
+++ model/dinner.rb
@@ -2,23 +2,20 @@ require 'mongo_mapper'
 
 class Dinner
   include MongoMapper::Document
+
   #Attributes
-  key :_id, ObjectId
+  key :_id
   key :title, String, :required => true
   key :event_date, Date
   key :description, String, :required => true
   key :hosted_by, String, :required => true
-  key :contact_phone, String
+  key :contact_phone, String, :required => true
   key :address, String, :required => true
   key :country, String, :required => true
-  key :latitude, Double, :required => true
-  key :longitude, Double, :required => true
+  key :latitude, Float
+  key :longitude, Float
   timestamps!
 
-  #Validations
-  RegPhoneNum = "^[2-9]\\d{2}-\\d{3}-\\d{4}$"
-  validates_format_of :contact_phone, with => RegPhoneNum, :allow_blank => false
-
   #Associations
-  many :rsvps
+  #many :rsvps
 end
